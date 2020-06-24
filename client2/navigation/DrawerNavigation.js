f3383585b408b8e0d8dd1c9b7a7c19f650736e48
thezano <fauzan.habib@outlook.com>
2020-06-24 16:44:36 +0700

    scaffold wishlist ui

--- client2/navigation/DrawerNavigation.js
+++ client2/navigation/DrawerNavigation.js
@@ -1,21 +1,30 @@
 import * as React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import VenuesNavigator from './VenuesNavigator';
 import ProfileNavigator from './ProfileNavigator';
 import ReservationNavigator from './ReservationNavigator';
 import OrderNavigator from './OrderNavigator';
+import WishListsScreen from '../screens/WishListsScreen';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { COLORS } from '../utils/colors';
 
 const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen options={{tabBarIcon: ({focused}) => <Icon name='building' size={24} color={focused ? COLORS.main : COLORS.disabled} /> }} name="Venues" component={VenuesNavigator} />
+      <Tab.Screen options={{tabBarIcon: ({focused}) => <Icon name='ticket-alt' size={24} color={focused ? COLORS.main : COLORS.disabled} /> }} name="Reservations" component={ReservationNavigator} />
+      <Tab.Screen options={{tabBarIcon: ({focused}) => <Icon name='heart' size={24} color={focused ? COLORS.main : COLORS.disabled} /> }} name="Wishlists" component={WishListsScreen} />
+    </Tab.Navigator>
+  );
+};
 export default () => (
-  <Drawer.Navigator initialRouteName="Venues" drawerPosition="right">
-    <Drawer.Screen
-      name="Venues"
-      component={VenuesNavigator}
-
-    />
+  <Drawer.Navigator initialRouteName="Home" drawerPosition="right">
+    <Drawer.Screen name="Home" component={TabNavigator} />
+    <Drawer.Screen name="Orders" component={OrderNavigator} />
     <Drawer.Screen name="Profile" component={ProfileNavigator} />
-    <Drawer.Screen name="Reservations" component={ReservationNavigator} />
-    <Drawer.Screen name='Orders' component={OrderNavigator} />
   </Drawer.Navigator>
 );
---
+++ client2/screens/WishListsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const WishListsScreen = () => {
+    return (
+        <View>
+            <Text>wishlists</Text>
+        </View>
+    )
+}
+
+export default WishListsScreen
+
+const styles = StyleSheet.create({})
