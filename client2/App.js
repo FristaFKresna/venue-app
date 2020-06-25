b0095780c0e7d584a1ae4f439bae028cc81a5565
thezano <fauzan.habib@outlook.com>
2020-06-25 09:27:39 +0700

    finish persist user

--- client2/App.js
+++ client2/App.js
@@ -1,11 +1,14 @@
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import * as React from 'react';
-import { StatusBar, StyleSheet, View, Text } from 'react-native';
+import { StatusBar, StyleSheet, View, Text} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage'
 import AuthNavigator from './navigation/AuthNavigator';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import withStore from './hocs/withStore';
 import DrawerNavigation from './navigation/DrawerNavigation';
 import VerificationScreen from './screens/VerificationScreen';
+import { setToken, api } from './utils/axios';
+import { SET_USER } from './store/actions/actionTypes';
 
 const MyTheme = {
   ...DefaultTheme,
@@ -16,6 +19,19 @@ const MyTheme = {
 };
 
 function App(props) {
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    AsyncStorage.getItem('token')
+      .then((token) => {
+        if (token) {
+          setToken(token);
+          return api.post('/auth/deserialize');
+        }
+      })
+      .then(({ data }) => {
+        dispatch({ type: SET_USER, payload: { ...data } });
+      });
+  }, []);
   const auth = useSelector((state) => state.auth);
   return (
     <View style={styles.container}>
