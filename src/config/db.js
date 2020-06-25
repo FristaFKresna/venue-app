aebd9a3e3bf4b7cba7a9b23689f60f04df0b89ac
thezano <fauzan.habib@outlook.com>
2020-06-25 10:44:00 +0700

    fix static port

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ const {MYSQL_USER, MYSQL_PASSWORD, MYSQL_DBNAME} = process.env
 const sequelize = new Sequelize(MYSQL_DBNAME, MYSQL_USER, MYSQL_PASSWORD, {
     dialect: 'mysql',
     host: 'localhost',
-    port: 3307,
+    port: process.env.DB_PORT,
     define: {
         timestamps: false,
     }
